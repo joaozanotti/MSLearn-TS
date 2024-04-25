e60f069b5e15d975f2ebf857f052b62ab6dbd65a
João Vitor Zanotti <108589098+joaozanotti@users.noreply.github.com>
2024-04-25 17:05:23 -0300

    Finalização dos exercícios do primeiro módulo

--- modulo-1/modulo01.ts
+++ modulo-1/modulo01.ts
@@ -1,4 +1,8 @@
+/* Module 1: Introduction to TypeScript */
+
+/*  Exercises  */
+
 function addNumbers(x: number, y: number) {
     return x + y;
-  }
-  console.log(addNumbers(3, 6));
+}
+console.log(addNumbers(3, 6));
